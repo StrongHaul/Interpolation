0f596593b7daee33d56ee0c202c92dd5ddf8d1bd
StrongHaul <usermage@gmail.com>
2019-12-13 11:39:08 +0500

    1

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.z1 = new ZedGraph.ZedGraphControl();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// z1
+			// 
+			this.z1.AutoScroll = true;
+			this.z1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.z1.Location = new System.Drawing.Point(306, 0);
+			this.z1.Margin = new System.Windows.Forms.Padding(5);
+			this.z1.Name = "z1";
+			this.z1.ScrollGrace = 0D;
+			this.z1.ScrollMaxX = 0D;
+			this.z1.ScrollMaxY = 0D;
+			this.z1.ScrollMaxY2 = 0D;
+			this.z1.ScrollMinX = 0D;
+			this.z1.ScrollMinY = 0D;
+			this.z1.ScrollMinY2 = 0D;
+			this.z1.Size = new System.Drawing.Size(717, 581);
+			this.z1.TabIndex = 0;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(135, 447);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(0, 17);
+			this.label10.TabIndex = 14;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(15, 533);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(269, 37);
+			this.button1.TabIndex = 17;
+			this.button1.Text = "Построить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+			this.dataGridView1.Location = new System.Drawing.Point(15, 15);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.Size = new System.Drawing.Size(281, 361);
+			this.dataGridView1.TabIndex = 20;
+			// 
+			// Column1
+			// 
+			this.Column1.FillWeight = 60.9137F;
+			this.Column1.HeaderText = "x";
+			this.Column1.MinimumWidth = 6;
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			// 
+			// Column2
+			// 
+			dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.Column2.DefaultCellStyle = dataGridViewCellStyle3;
+			this.Column2.FillWeight = 113.0288F;
+			this.Column2.HeaderText = "y";
+			this.Column2.MinimumWidth = 6;
+			this.Column2.Name = "Column2";
+			this.Column2.ReadOnly = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(142, 400);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(142, 53);
+			this.button2.TabIndex = 25;
+			this.button2.Text = "Файл с исх. данными";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(15, 400);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(14, 17);
+			this.label1.TabIndex = 26;
+			this.label1.Text = "x";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(41, 400);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(68, 22);
+			this.textBox1.TabIndex = 27;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 442);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(15, 17);
+			this.label2.TabIndex = 28;
+			this.label2.Text = "y";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(41, 438);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(68, 22);
+			this.textBox2.TabIndex = 29;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Канонический",
+            "Кусочно-линейный",
+            "Лагранжа",
+            "Ньютона"});
+			this.comboBox1.Location = new System.Drawing.Point(116, 494);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(168, 24);
+			this.comboBox1.TabIndex = 30;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(181, 457);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(103, 34);
+			this.label3.TabIndex = 31;
+			this.label3.Text = "Метод\r\nинтерполяции\r\n";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.button1;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1023, 581);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.z1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Form1";
+			this.Text = "Лаб 2. Интерполяция значений";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl z1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Linq;
+using System.Collections;
+using System.IO;
+
+
+namespace lab2
+{
+	public partial class Form1 : Form
+	{
+		double[] X = new double[1];
+		double[] Y = new double[1];
+		static double[] Xinter = new double[0];
+		double[] Yinter = new double[0];
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			GraphPane pane = z1.GraphPane;
+
+			z1.GraphPane.CurveList.Clear();
+			z1.IsShowPointValues = true;
+			z1.IsEnableHZoom = true;
+			z1.IsEnableVZoom = true;
+
+			pane.XAxis.Title.IsVisible = false;
+			pane.YAxis.Title.IsVisible = false;
+			pane.XAxis.Scale.IsSkipFirstLabel = true;
+			pane.XAxis.Scale.IsSkipLastLabel = true;
+			pane.XAxis.Scale.IsSkipCrossLabel = true;
+			pane.YAxis.Scale.IsSkipLastLabel = true;
+			pane.YAxis.Scale.IsSkipCrossLabel = true;
+			pane.Title.IsVisible = false;
+
+			pane.XAxis.Cross = 0.0;
+			pane.YAxis.Cross = 0.0;
+
+			//pane.XAxis.Scale.Min = X.Min();       // левая граница масштаба
+			//pane.XAxis.Scale.Max = X.Max();       // правая граница масштаба
+			//pane.YAxis.Scale.Min = Y.Min();      // По оси Y установим автоматический подбор масштаба
+			//pane.YAxis.Scale.Max = Y.Max();
+
+			pane.IsBoundedRanges = true;    // при автоматическом подборе масштаба нужно учитывать только видимый интервал графика
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+		private void Button2_Click(object sender, EventArgs e)      // выбрать файл
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+				return;
+			dataGridView1.Rows.Clear();
+			Array.Resize(ref X, 0);             // обнулили длину массива   
+			Array.Resize(ref Y, 0);
+
+			string filename = openFileDialog1.FileName;
+			StreamReader filereader = new StreamReader(filename);
+			int i = 0;
+			while (filereader.EndOfStream != true)
+			{
+				string[] line = filereader.ReadLine().Replace(" ", "").Split(new string[] { "," }, StringSplitOptions.None);     // строка файла "x, y"
+				Array.Resize(ref X, X.Length + 1);             // увеличили длину массива на 1 
+				Array.Resize(ref Y, Y.Length + 1);
+				X[i] = Convert.ToDouble(line[0]);
+				Y[i] = Convert.ToDouble(line[1]);
+				dataGridView1.Rows.Add(X[i], Y[i]);
+
+				//dataGridView1.Rows[i].Cells[0].Value = X[i];    // добавить x в таблицу
+				//dataGridView1.Rows[i].Cells[1].Value = Y[i];    // добавить y в таблицу
+				i++;
+			}
+			filereader.Close();
+		}
+
+		private void button1_Click(object sender, EventArgs e)      // построить
+		{
+			z1.GraphPane.CurveList.Clear();
+			double x = Convert.ToDouble(textBox1.Text);
+
+			Array.Resize(ref Xinter, 0);
+			Array.Resize(ref Yinter, 0);
+
+			var res = (0.0, new double[Xinter.Length]);
+			switch (comboBox1.SelectedIndex)
+			{
+				case 0:
+					res = canon(x);
+					break;
+				case 1:
+					res = liney(x);
+					break;
+				case 2:
+					res = lagrange(x);
+					break;
+				case 3:
+					res = newton(x);
+					break;
+			}
+
+			textBox2.Text = Math.Round(res.Item1, 2).ToString();        // вывести высчитанную Y точку
+			res.Item2.CopyTo(Yinter, 0);            // скопировать данные в массив Yinter
+
+			LineItem points, line;
+			points = z1.GraphPane.AddCurve("Исх. данные", X, Y, Color.Green, SymbolType.Circle);       // точки
+			points.Symbol.Fill.Color = Color.Green;         // цвет заливки
+			points.Symbol.Fill.Type = FillType.Solid;       // тип заливки
+			points.Line.IsVisible = false;
+
+			line = z1.GraphPane.AddCurve("Интерполяция", Xinter, Yinter, Color.Blue, SymbolType.None);       // линия
+			line.Line.Width = 2;
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+
+		public (double, double[]) canon(double x)       // каноническая
+		{
+			for (double i = 0, k = 1.0; k <= 6.0; k += 0.1, i++)
+			{
+				Array.Resize(ref Xinter, Xinter.Length + 1);
+				Xinter[(int)i] = k;
+			}
+			Array.Resize(ref Yinter, Xinter.Length);
+
+			int N = X.Length;
+
+			(double, double[]) res = (0, new double[Xinter.Length]);     // кортеж (Yточка, Yинтерп)
+			double[] A = new double[N];                          // массив для хранения коэфф А
+
+			double[,] Xmatrix = new double[N, N + 1];       // расширенная матрица
+			for (int i = 0; i < N; i++)                     // заполняем расширенную матрицу
+				for (int j = 0; j < N + 1; j++)
+				{
+					if (j < N)
+						Xmatrix[i, j] = Math.Pow(X[i], j);
+					else
+						Xmatrix[i, j] = Y[i];
+				}
+
+			// прямой ход
+			for (int i = 0; i < N - 1; ++i)
+			{
+				double element_of_glavn = Xmatrix[i, i];       // элемент главной диагонали
+
+				for (int s = i + 1; s < N; s++)
+				{
+					double obnul = Xmatrix[s, i];       // элемент строки который нужно обнулить
+					for (int j = i; j < N + 1; j++)
+						Xmatrix[s, j] += Xmatrix[i, j] * (-obnul / element_of_glavn);
+				}
+			}
+
+			//обратный ход            
+			for (int i = N - 1; i > 0; i--)
+			{
+				double element_of_glavn = Xmatrix[i, i];       // элемент главной диагонали
+
+				for (int s = i - 1; s >= 0; s--)
+				{
+					double obnul = Xmatrix[s, i];       // элемент строки который нужно обнулить
+					for (int j = N; j >= 0; j--)
+						Xmatrix[s, j] += Xmatrix[i, j] * (-obnul / element_of_glavn);
+				}
+			}
+
+			for (int i = 0; i < N; i++)
+			{
+				A[i] = Xmatrix[i, N] / Xmatrix[i, i];
+			}
+
+			for (int i = 0; i < Xinter.Length; i++)
+				for (int j = 0; j < N; j++)
+					res.Item2[i] += A[j] * Math.Pow(Xinter[i], j);
+
+			for (int j = 0; j < N; j++)
+				res.Item1 += A[j] * Math.Pow(x, j);
+
+			return res;
+		}
+
+		public (double, double[]) liney(double x)       // линейная
+		{
+			for (double i = 0, k = 1.0; k <= 6.0; k += 0.5, i++)
+			{
+				Array.Resize(ref Xinter, Xinter.Length + 1);
+				Xinter[(int)i] = k;
+			}
+			Array.Resize(ref Yinter, Xinter.Length);
+
+			(double, double[]) res = (0, new double[Xinter.Length]);     // кортеж (Yточка, Yинтерп)
+
+			double[] A = new double[X.Length];
+			double[] B = new double[X.Length];
+
+			for (int i = 0; i < X.Length - 1; i++)
+			{
+				A[i] = (Y[i + 1] - Y[i]) / (X[i + 1] - X[i]);
+				B[i] = Y[i] - A[i] * X[i];
+				res.Item2[i * 2] = Y[i];                    // [1] 2 3
+				res.Item2[i * 2 + 1] = A[i] * Xinter[i * 2 + 1] + B[i];     // 1 [2] 3
+				res.Item2[i * 2 + 2] = Y[i + 1];                // 1 2 [3]
+				if (X[i] <= x && x <= X[i + 1])
+					res.Item1 = A[i] * x + B[i];
+			}
+
+			return res;
+		}
+
+		public (double, double[]) lagrange(double x)    // лагранж
+		{
+			for (double i = 0, k = 1.0; k <= 6.0; k += 0.1, i++)
+			{
+				Array.Resize(ref Xinter, Xinter.Length + 1);
+				Xinter[(int)i] = k;
+			}
+			Array.Resize(ref Yinter, Xinter.Length);
+
+			(double, double[]) res = (0, new double[Xinter.Length]);     // кортеж (Yточка, Yинтерп)
+
+			for (int i = 0; i < Xinter.Length; i++)
+				res.Item2[i] = lagrangeforpoint(Xinter[i], X, Y, X.Length);
+
+			double lagrangeforpoint(double xinter, double[] xValues, double[] yValues, int size)
+			{
+				double lagrangePol = 0;
+				for (int i = 0; i < size; i++)
+				{
+					double basicsPol = 1;
+
+					for (int j = 0; j < size; j++)
+						if (j != i)
+							basicsPol *= (xinter - xValues[j]) / (xValues[i] - xValues[j]);
+
+					lagrangePol += basicsPol * yValues[i];
+				}
+				return lagrangePol;
+			}
+			res.Item1 = lagrangeforpoint(x, X, Y, X.Length);
+			return res;
+		}
+
+		public (double, double[]) newton(double x)      // ньютон
+		{
+			for (double i = 0, k = 1.0; k <= 6.0; k += 0.1, i++)
+			{
+				Array.Resize(ref Xinter, Xinter.Length + 1);
+				Xinter[(int)i] = k;
+			}
+			Array.Resize(ref Yinter, Xinter.Length);
+
+			(double, double[]) res = (0, new double[Xinter.Length]);     // кортеж (Yточка, Yинтерп)
+
+			for (int i = 0; i < Xinter.Length; i++)
+				res.Item2[i] = newton_for_point(Xinter[i], X.Length - 1, X, Y, 1);
+
+			double newton_for_point(double xinter, int n, double[] MasX, double[] MasY, double step)
+			{
+				double[,] mas = new double[n + 2, n + 1];
+				for (int i = 0; i < 2; i++)
+				{
+					for (int j = 0; j < n + 1; j++)
+					{
+						if (i == 0)
+							mas[i, j] = MasX[j];
+						else if (i == 1)
+							mas[i, j] = MasY[j];
+					}
+				}
+				int m = n;
+				for (int i = 2; i < n + 2; i++)
+				{
+					for (int j = 0; j < m; j++)
+					{
+						mas[i, j] = mas[i - 1, j + 1] - mas[i - 1, j];
+					}
+					m--;
+				}
+
+				double[] dy0 = new double[n + 1];
+
+				for (int i = 0; i < n + 1; i++)
+				{
+					dy0[i] = mas[i + 1, 0];
+				}
+
+				double result = dy0[0];
+				double[] xn = new double[n];
+				xn[0] = xinter - mas[0, 0];
+
+				for (int i = 1; i < n; i++)
+				{
+					double ans = xn[i - 1] * (xinter - mas[0, i]);
+					xn[i] = ans;
+					ans = 0;
+				}
+
+				int m1 = n + 1;
+				int fact = 1;
+				for (int i = 1; i < m1; i++)
+				{
+					fact = fact * i;
+					result = result + (dy0[i] * xn[i - 1]) / (fact * Math.Pow(step, i));
+				}
+
+				return result;
+			}
+			res.Item1 = newton_for_point(x, X.Length - 1, X, Y, 1);
+			return res;
+		}
+
+	}
+}
